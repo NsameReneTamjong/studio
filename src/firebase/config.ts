2919e5402f9af8d745bdde87537606f43979748b
Nsame Rene Tamjong <nsamerenetamjong@gmail.com>
2026-03-23 01:16:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7858928176-b2a80",
+  "appId": "1:721326746339:web:9ab5313092e4ccb32773d6",
+  "apiKey": "AIzaSyBh4b2FNAwZ1-kVgTTvAPxo1puL37v09qc",
+  "authDomain": "studio-7858928176-b2a80.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "721326746339"
+};
